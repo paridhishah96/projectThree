ff4a5bb39873fe6f82f839f172a624fef8f2aa03
paridhishah96 <paridhishah96@gmail.com>
2022-08-09 16:13:59 -0400

    got the API to work and basic info to display on page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+//  https://quiet-ridge-74718.herokuapp.com/
+// api key f1fc157fc587725a2aa64311ce48541f;
+// const apiUrl = "https://quiet-ridge-74718.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search";
+const apiKey = "f1fc157fc587725a2aa64311ce48541f"
+// const url = new URL(apiUrl);
+// url.search = new URLSearchParams({
+//     apikey: apiKey,
+//     q_track: "shake it off",
+//     q_artist: "taylor swift"
+// });
+// fetch(url)
+//     .then(function (response) {
+//         if (response.ok) {
+//             return response.json();
+//         } else {
+//             throw new Error();
+//         }
+//     })
+//         .then(function (jsonData) {
+//         console.log(jsonData);
+//         })
+//         .catch(function () {
+//         console.log("there's an error :(")
+//         })
+
+const apiUrlTwo = "https://quiet-ridge-74718.herokuapp.com/http://api.musixmatch.com/ws/1.1/matcher.track.get";
+
+
 function App() {
+const [ userInput, setUserInput] = useState("");
+const [genre, setGenre] = useState([])
+
+function handleInputChange (e) {
+  setUserInput(e.target.value)
+}
+
+function handleClick (e) {
+	e.preventDefault();
+
+	const urlTwo = new URL(apiUrlTwo);
+	urlTwo.search = new URLSearchParams({
+		apikey: apiKey,
+		// track_id:  74376191,
+		q_track: "Bohemian Rhapsody",
+		q_artist: "Queen"
+});
+  fetch(urlTwo)
+    .then(function (response) {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error();
+        }
+    })
+        .then(function (jsonData) {
+        // console.log(jsonData.message.body.track.primary_genres.music_genre_list[0].music_genre.music_genre_name);
+        setGenre(jsonData.message.body.track.primary_genres.music_genre_list[0].music_genre.music_genre_name)
+        })
+        .catch(function () {
+        console.log("there's an error :(")
+        })
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React app</h1>
+      <form>
+        <label htmlFor="songName">Song title</label>
+        <input onChange={handleInputChange}  type="songName" name="songName" />
+
+        {/* <label htmlFor="songName">Artist</label>
+        <input onChange={handleInputChange}  type="songName" name="songName" /> */}
+
+        <button onClick={handleClick}> Click to see the genres</button>
+      </form>
+      <p>Genre of the song is: {genre}</p>
     </div>
   );
 }
